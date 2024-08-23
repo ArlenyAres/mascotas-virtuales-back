43094f97d8c31800d86ad71771c2e0de1202108a
Arleny <arleny.ares@gmail.com>
2024-08-23 12:54:50 +0200

    first commit

---
+++ src/main/java/com/mascotas_virtuales/mascotas_virtuales/MascotasVirtualesBackApplication.java
@@ -0,0 +1,13 @@
+package com.mascotas_virtuales.mascotas_virtuales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascotasVirtualesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascotasVirtualesBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mascotas_virtuales/mascotas_virtuales/MascotasVirtualesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mascotas_virtuales.mascotas_virtuales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotasVirtualesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
